c937c53fde5319d2f7893f34ee622473233d6281
JerryFlop <JParedes@wil.yearup.org>
2023-03-25 19:49:21 -0400

    first commit

---
+++ src/main/java/Homework/Fifteen.java
@@ -0,0 +1,50 @@
+package Homework;
+
+public class Fifteen {
+//question 1
+    public static int getLastIndex(String[] names) {
+        System.out.println(names.length -1);
+        return 0;
+    }
+
+
+    // Question 2
+//    public static int getSecondToLastIndex(String[] names) {
+//        return 0;
+//    }
+//    //Question 3
+//    public static String getFirstElement(String[] names) {
+//        return null;
+//    }
+//    // Question 4
+//    public static String getLastElement(String[] names) {
+//
+//        return null;
+//    }
+//    // Question 5
+//    public static String getSecondToLastElement(String[] names) {
+//        return null;
+//    }
+//   // Question 6
+//    public static int getSum(int[] ints) {
+//        return 0;
+//    }
+
+    public static void main(String[] args) {
+
+        String[] names = {"Martin", "JOhn", "jimmy", "jeff", "casey", "david"};
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(getLastIndex(names));
+//        System.out.println(names.length - 1);
+//        System.out.println(names.length - 2);
+//        System.out.println(names[0]);
+//        System.out.println(names[names.length-1]);
+//        System.out.println(names[4]);
+
+
+
+
+
+
+    }
+}
