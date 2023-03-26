fdc5bb726cf6549c7ac80bc0babd5601b58abd58
JerryFlop <JParedes@wil.yearup.org>
2023-03-26 00:28:17 -0400

    second commit

--- src/main/java/Homework/Fifteen.java
+++ src/main/java/Homework/Fifteen.java
@@ -3,24 +3,24 @@ package Homework;
 public class Fifteen {
 //question 1
     public static int getLastIndex(String[] names) {
-        System.out.println(names.length -1);
-        return 0;
+
+        return names.length - 1 ;
     }
 
 
     // Question 2
-//    public static int getSecondToLastIndex(String[] names) {
-//        return 0;
-//    }
+    public static int getSecondToLastIndex(String[] names) {
+        return names.length - 2;
+    }
 //    //Question 3
-//    public static String getFirstElement(String[] names) {
-//        return null;
-//    }
+    public static String getFirstElement(String[] names) {
+        return names[0];
+    }
 //    // Question 4
-//    public static String getLastElement(String[] names) {
-//
-//        return null;
-//    }
+   public static String getLastElement(String[] names) {
+
+       return names[names.length-1];
+     }
 //    // Question 5
 //    public static String getSecondToLastElement(String[] names) {
 //        return null;
@@ -35,8 +35,9 @@ public class Fifteen {
         String[] names = {"Martin", "JOhn", "jimmy", "jeff", "casey", "david"};
         int[] nums = {1,2,3,4,5,6,7,8,9,10};
         System.out.println(getLastIndex(names));
-//        System.out.println(names.length - 1);
-//        System.out.println(names.length - 2);
+        System.out.println(getSecondToLastIndex(names));
+        System.out.println(getFirstElement(names));
+        System.out.println(getLastElement(names));
 //        System.out.println(names[0]);
 //        System.out.println(names[names.length-1]);
 //        System.out.println(names[4]);
